8c1c9218e334d08574cb5229e5943f7ab1206420
JakobusV <vajakous@gmail.com>
2021-03-04 15:04:35 -0700

    Added EditText and Filter methods to SearchUsersActivity

--- app/src/main/java/com/neumontmc/stats_app/View/SearchUsersActivity.java
+++ app/src/main/java/com/neumontmc/stats_app/View/SearchUsersActivity.java
@@ -8,12 +8,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class SearchUsersActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
 
-    String names[], uuids[];
+    String[] names, uuids;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +30,42 @@ public class SearchUsersActivity extends AppCompatActivity {
         //names = //Get names
         //uuids = //Get uuids
 
-        SearchAdapter sAdapter = new SearchAdapter(this, names, uuids);
+        genViewer(names);
+
+        EditText searchUserET = findViewById(R.id.searchUserET);
+        searchUserET.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                filter(s.toString());
+            }
+        });
+    }
+
+    private void filter(String text){
+        ArrayList<String> filteredList = new ArrayList<>();
+
+        for (String string : names){
+            if (string.toLowerCase().contains(text.toLowerCase())){
+                filteredList.add(string);
+
+            }
+        }
+
+        genViewer((String[])filteredList.toArray());
+    }
+
+    private void genViewer(String[] currentNames){
+        SearchAdapter sAdapter = new SearchAdapter(this, currentNames, uuids);
         recyclerView.setAdapter(sAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
